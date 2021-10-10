48c2d418cea403afbf8220df285e8872415d0f63
RAFSAN1020 <kabirrafsan10@gmail.com>
2021-10-11 01:52:24 +0600

    ur comment

---
+++ lab task3/profile/profilepicture.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+	<fieldset>
+		<form>
+			<label for="myfile">Select a file</label>
+			<input type="file" id="myfile" name="myfile">
+		
+			<br>
+			<hr size="8" width="25%" color= "gray" text align="left">	
+			<input type="submit" name="submit" value="submit">
+			</form>
+	</fieldset>
+
+</body>
+</html>
